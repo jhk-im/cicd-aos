a58dc1b7710772e551e473ce32a27e6866090cc4
jhk-im <kjhun200@gmail.com>
2023-02-01 19:26:05 +0900

    release(play-store): version 1.2

--- app/src/main/java/com/jhk/cicd_aos/MainActivity.kt
+++ app/src/main/java/com/jhk/cicd_aos/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.jhk.cicd_aos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
